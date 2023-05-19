23c1b17e30acabe4ab89aab1b07fe3454ddbfe02
React1 <React1@flyweis.technology>
2023-05-19 17:46:23 +0530

    cahnges

---
+++ .history/index_20230519174348.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");  
+const cors = require("cors");
+
+const app = express();
+const bodyparser = require("body-parser");
+// const serverless = require('serverless-http')
+const userRouter = require("./src/route/user")
+
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 2023;
+
+mongoose
+  .connect("mongodb+srv://sumitnegi:<pWnikbrRlT68LBeY>@cluster0.diszcfl.mongodb.net/")
+  .then(() => {
+    console.log("Db conneted succesfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+  app.get('/home',(req,res)=>{
+res.send("hello world")
+  })
+app.use("/api/v1", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+// module.exports = {
+//   handler: serverless(app)
+// }
